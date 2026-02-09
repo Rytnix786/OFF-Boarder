232a4cf0ab7f444117d309abf0f64900ac844535
Mehedi D Nafis <nafismehedi37@gmail.com>
2026-02-09 10:37:58 +0600

    Add prisma.config.ts

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -2,8 +2,7 @@ export default {
   datasources: {
     db: {
       url: process.env.DIRECT_DATABASE_URL || 
-           process.env.DATABASE_URL ||
-           "postgresql://postgres.mcmqzwgaojgmrcmdsygh:spLavB1IJzRrybp9R0AsQT0xpSKYsxMCaV8bmlUVvHSVAhRwOKc2A1yjNpo3oyyM@aws-0-us-west-2.pooler.supabase.com:6543/postgres?pgbouncer=true",
+           "postgresql://postgres:password@localhost:5432/offboarder",
     },
   },
 };
